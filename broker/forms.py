5a93ffbe5511632343c52533c146cb89daad15a8
Daryl Dudey <daryljdudey@googlemail.com>
2020-02-19 21:43:11 +0000

    company creation attempt

--- broker/forms.py
+++ broker/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Company
 
+
 class CompanyForm(forms.Form):
     name = forms.CharField(label='Company name', max_length=100)
+    email = forms.EmailField(label='Contact email')
--- broker/models.py
+++ broker/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Company(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(blank=True)
+    email = models.EmailField(blank=False)
 
 
 class Project(models.Model):
